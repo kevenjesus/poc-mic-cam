799b0fdecf74934fc6bc1f72b2375ad8d2a3a761
Keven Jesus <knjo@gft.com>
2021-02-16 13:56:24 -0300

    poc

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { useEffect, useRef } from 'react'
+import { useRef, useCallback } from 'react'
 
 function App() {
 
   const videoRef = useRef();
 
-  useEffect(() => {
-    const getWebCam = async () => {
-      await navigator.mediaDevices
-          .getUserMedia({ video: true, audio: true })
-          .then(stream => {
-              videoRef.current.srcObject = stream
-          })
-          .catch(error => {
-              console.error('Erro ao iniciar permissoes', error)
-          })
-    }
+  const succesVideo = useCallback((stream) => {
+    console.log('success');
+    videoRef.current.srcObject = stream
+  }, [])
 
-    getWebCam();
-  })
+  const errorVideo = useCallback((error) => {
+    console.log('falha', error);
+  }, [])
+
+  const getWebCam = useCallback(async () => {
+    await navigator.getUserMedia({ video: true, audio: true }, succesVideo, errorVideo)
+  }, [succesVideo, errorVideo])
+
   return (
-    <div style={{display: 'flex', width: '100%', height:'100vh', justifyContent: 'center', alignItems: 'center'}}>
+    <div style={{display: 'flex', flexDirection: 'column', width: '100%', height:'100vh', justifyContent: 'center', alignItems: 'center'}}>
       <h1>App carregou</h1>
       <video ref={videoRef} width="320" playsInline autoPlay />
+      <button onClick={getWebCam} style={{fontSize: 20}}>Get permissions</button>
     </div>
   );
 }
